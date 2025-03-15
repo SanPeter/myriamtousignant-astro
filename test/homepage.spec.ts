ab3b8197fd597ad109468d2b6d9fac91668f6344
SanPeter <stpierre.eric@gmail.com>
2025-03-15 09:56:42 -0400

    Add homepage test and remove tests not needed

--- test/homepage.spec.ts
+++ test/homepage.spec.ts
@@ -6,3 +6,11 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Hello, Astro!/);
 });
+
+test('has heading', async ({ page }) => {
+  await page.goto('http://localhost:4321/');
+
+  // Expect at least one h1 to have the text "Hello World!".
+  const h1Element = page.locator('h1').filter({ hasText: 'Hello, World!' }).filter();
+  await expect(h1Element).toHaveText('Hello, World!');
+});
