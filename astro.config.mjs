a3d805cd514349aef4dec64d5f38201d990eec97
SanPeter <stpierre.eric@gmail.com>
2025-03-19 09:04:54 -0400

    Add publication step

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'http://astro.myriamtousignant.com',
     vite: {
         plugins: [tailwindcss()],
     },
