b7799c74c0f2b277ee4a7296157172297b1391d0
SanPeter <stpierre.eric@gmail.com>
2025-03-19 20:26:56 -0400

    Homepage initial design

--- e2e/homepage.spec.ts
+++ e2e/homepage.spec.ts
@@ -4,32 +4,24 @@ test('has title', async ({ page }) => {
   await page.goto('http://localhost:4321/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Hello, Astro!/);
+  await expect(page).toHaveTitle(/Myriam Tousignant/);
 });
 
-test('has heading', async ({ page }) => {
-  await page.goto('http://localhost:4321/');
+// test('h1 has correct color', async ({ page }) => {
+//   await page.goto('http://localhost:4321/');
 
-  // Expect at least one h1 to have the text "Hello World!".
-  const h1Element = page.locator('h1').filter({ hasText: 'Hello, World!' }).filter();
-  await expect(h1Element).toHaveText('Hello, World!');
-});
+//   // Expect the h1 with text "Hello World!" to have the color oklch(0.623 0.214 259.815).
+//   const h1Element = page.locator('h1').filter({ hasText: 'Hello, World!' }).first();
+//   const color = await h1Element.evaluate((element) => getComputedStyle(element).color);
+//   expect(color).toBe('oklch(0.623 0.214 259.815)');
+// });
 
-test('h1 has correct color', async ({ page }) => {
-  await page.goto('http://localhost:4321/');
+// test('h1 has correct classes', async ({ page }) => {
+//   await page.goto('http://localhost:4321/');
 
-  // Expect the h1 with text "Hello World!" to have the color oklch(0.623 0.214 259.815).
-  const h1Element = page.locator('h1').filter({ hasText: 'Hello, World!' }).first();
-  const color = await h1Element.evaluate((element) => getComputedStyle(element).color);
-  expect(color).toBe('oklch(0.623 0.214 259.815)');
-});
-
-test('h1 has correct classes', async ({ page }) => {
-  await page.goto('http://localhost:4321/');
-
-  // Expect the h1 with text "Hello World!" to have classes "text-3xl" and "text-blue-500".
-  const h1Element = page.locator('h1').filter({ hasText: 'Hello, World!' }).first();
-  const classList = await h1Element.evaluate((element) => Array.from(element.classList));
-  expect(classList).toContain('text-3xl');
-  expect(classList).toContain('text-blue-500');
-});
+//   // Expect the h1 with text "Hello World!" to have classes "text-3xl" and "text-blue-500".
+//   const h1Element = page.locator('h1').filter({ hasText: 'Hello, World!' }).first();
+//   const classList = await h1Element.evaluate((element) => Array.from(element.classList));
+//   expect(classList).toContain('text-3xl');
+//   expect(classList).toContain('text-blue-500');
+// });
