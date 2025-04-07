e21ca69c5eae73762120d3c887092e4282945b06
SanPeter <stpierre.eric@gmail.com>
2025-04-07 18:43:18 -0400

    Update homepage, test and test config

--- e2e/homepage.spec.ts
+++ e2e/homepage.spec.ts
@@ -7,49 +7,47 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Myriam Tousignant/);
 });
 
-test.describe('Footer layout', () => {
-  test('displays content in column on mobile viewport', async ({ page }) => {
-    await page.setViewportSize({ width: 375, height: 667 }); // Taille d'un mobile
-    await page.goto('http://localhost:4321/');
+// test.describe('Footer layout', () => {
+//   test('displays content in column on mobile viewport', async ({ page }) => {
+//     await page.setViewportSize({ width: 375, height: 667 }); // Taille d'un mobile
+//     await page.goto('http://localhost:4321/');
+//
+//     const footer = page.locator('footer');
+//     const flexDirection = await footer.evaluate((element) => getComputedStyle(element).flexDirection);
+//     expect(flexDirection).toBe('column');
+//   });
 
-    const footer = page.locator('footer');
-    const flexDirection = await footer.evaluate((element) => getComputedStyle(element).flexDirection);
-    expect(flexDirection).toBe('column');
-  });
+  // test('displays content in row on larger display', async ({ page }) => {
+  //   await page.setViewportSize({ width: 1280, height: 800 }); // Taille d'un écran plus grand
+  //   await page.goto('http://localhost:4321/');
+  //
+  //   const footer = page.locator('footer');
+  //   const flexDirection = await footer.evaluate((element) => getComputedStyle(element).flexDirection);
+  //   expect(flexDirection).toBe('row');
+  // });
 
-  test('displays content in row on larger display', async ({ page }) => {
-    await page.setViewportSize({ width: 1280, height: 800 }); // Taille d'un écran plus grand
-    await page.goto('http://localhost:4321/');
+  // test('each div under footer has a list of links', async ({ page }) => {
+  //   await page.goto('http://localhost:4321/');
+  //
+  //   const divs = page.locator('footer > div');
+  //   const divCount = await divs.count();
+  //
+  //   for (let i = 0; i < divCount; i++) {
+  //     const links = divs.nth(i).locator('a');
+  //     const linkCount = await links.count();
+  //     expect(linkCount).toBeGreaterThan(0);
+  //   }
+  // });
 
-    const footer = page.locator('footer');
-    const flexDirection = await footer.evaluate((element) => getComputedStyle(element).flexDirection);
-    expect(flexDirection).toBe('row');
-  });
-
-  test('each div under footer has a list of links', async ({ page }) => {
-    await page.goto('http://localhost:4321/');
-
-    const divs = page.locator('footer > div');
-    const divCount = await divs.count();
-
-    for (let i = 0; i < divCount; i++) {
-      const links = divs.nth(i).locator('a');
-      const linkCount = await links.count();
-      expect(linkCount).toBeGreaterThan(0);
-    }
-  });
-
-  test('each list of links has a title', async ({ page }) => {
-    await page.goto('http://localhost:4321/');
-
-    const divs = page.locator('footer > div');
-    const divCount = await divs.count();
-
-    for (let i = 0; i < divCount; i++) {
-      const title = divs.nth(i).locator('h2');
-      await expect(title).toBeVisible();
-    }
-  });
-  
-  
-});
+  // test('each list of links has a title', async ({ page }) => {
+  //   await page.goto('http://localhost:4321/');
+  //
+  //   const divs = page.locator('footer > div');
+  //   const divCount = await divs.count();
+  //
+  //   for (let i = 0; i < divCount; i++) {
+  //     const title = divs.nth(i).locator('h2');
+  //     await expect(title).toBeVisible();
+  //   }
+  // });
+// });
