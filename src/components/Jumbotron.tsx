45e8f5a4005a8bd6089332ba6066d9480f259fa1
SanPeter <stpierre.eric@gmail.com>
2025-05-17 16:38:08 -0400

    Jumbotron style update

--- src/components/Jumbotron.tsx
+++ src/components/Jumbotron.tsx
@@ -8,7 +8,7 @@ interface JumbotronProps {
 const Jumbotron: React.FC<JumbotronProps> = ({ title, description }) => {
   return (
     <div className="py-8 mb-10 border-b border-gray-200">
-      {title && <h1 className="text-4xl font-bold mb-3">{title}</h1>}
+      {title && <h1 className="text-6xl mb-3">{title}</h1>}
       {description && <p className="text-xl text-gray-600">{description}</p>}
     </div>
   );
