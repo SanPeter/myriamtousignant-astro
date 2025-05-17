58101685edeee288c56e874224e528d971de4a51
SanPeter <stpierre.eric@gmail.com>
2025-05-17 19:36:14 -0400

    Update content to use Instagram Grid

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,5 +1,1 @@
-
-import __ASTRO_IMAGE_IMPORT_Z22fvIY from "images/calq_logo.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fprojets%2Fresidence-ig-culture-monteregie%2Findex.md";
-import __ASTRO_IMAGE_IMPORT_Z1YQN9u from "images/culture_monteregie_couleur_Modif.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojets%2Fresidence-ig-culture-monteregie%2Findex.md";
-export default new Map([["images/calq_logo.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fprojets%2Fresidence-ig-culture-monteregie%2Findex.md", __ASTRO_IMAGE_IMPORT_Z22fvIY], ["images/culture_monteregie_couleur_Modif.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojets%2Fresidence-ig-culture-monteregie%2Findex.md", __ASTRO_IMAGE_IMPORT_Z1YQN9u]]);
-		
+export default new Map();
